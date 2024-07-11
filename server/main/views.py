63bca7eff848754594efcf9cd6c682979cbfec20
GitQuasar <cursedonela@yandex.ru>
2024-07-11 20:59:46 +0500

    Little redesign

--- server/main/views.py
+++ server/main/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def index(request) -> HttpResponse:
-#     return HttpResponse('Home Page')
-
 
 def index(request) -> HttpResponse:
     context = {
-        "title": "Home Page",
-        "content": "Welcome to the Home Page",
+        "title": "Home Comfort - Главная",
+        "content": "Магазин мебели Home Comfort",
     }
 
     return render(request, "main/index.html", context)
 
 
 def about(request) -> HttpResponse:
-    return HttpResponse("About Page")
+    context = {
+        "title": "Home Comfort - О нас",
+        "content": "О нас",
+        "text_on_page": "Здесь мы говорим, почему этот магазин такой классный, а также о том, какой хороший у нас товар",
+    }
+
+    return render(request, "main/about.html", context)
